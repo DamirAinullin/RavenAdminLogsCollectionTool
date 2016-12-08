770009500adf14b0164cf70b948ecea6e17a3b21
DamirAinullin <damirainullin@outlook.com>
2016-12-08 22:27:54 +0300

    Added Nunit test project and appveyor config

---
+++ RavenAdminLogsCollectionToolTests/MainViewModelTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace RavenAdminLogsCollectionToolTests
+{
+    [TestFixture]
+    public class MainViewModelTests
+    {
+        [Test]
+        public void LogsClearCommandTest()
+        {
+        }
+    }
+}
